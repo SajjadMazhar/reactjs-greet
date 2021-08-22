6dc8207403b5776481da1f718f4f4ba3d3c20828
Sajjad <sajjadthedevil123@gmail.com>
2021-08-22 12:50:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dateTimeToday = () => {
+
+  let currDate = new Date();
+  let nowH = currDate.getHours()
+  let nowTime = currDate.toLocaleTimeString();
+  return [nowH, nowTime];
 }
+const App = () => {
+  let greet = '';
+  let cssStyle = {};
+  if (dateTimeToday()[0] < 12 && dateTimeToday()[0] >= 4) {
+    greet = 'Good Morning!!';
+    cssStyle.color = 'rgb(113, 204, 231)';
+  } else if (dateTimeToday()[0] >= 12 && dateTimeToday()[0] < 17) {
+    greet = 'Good Afternoon!';
+    cssStyle.color = 'rgb(230, 218, 64)';
+  } else if (dateTimeToday()[0] >= 17 && dateTimeToday()[0] < 21) {
+    greet = 'Good Evening!';
+    cssStyle.color = 'rgb(230, 138, 19)';
+  } else {
+    greet = 'Good Night!';
+    cssStyle.color = 'rgb(104, 99, 93)'
+  }
 
+  return (
+    <> <div className="time"><h4>{dateTimeToday()[1]}</h4></div>
+      <div className="greeting">
+
+        <h1>Hello Sajjad, <span style={cssStyle}>{greet}</span></h1>
+      </div>
+    </>
+  )
+
+}
 export default App;
